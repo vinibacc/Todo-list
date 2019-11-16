a2f5180d22c8c87d9d444b92539c6734df90e9c5
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2019-11-16 18:57:27 -0300

    menu component

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,11 +4,13 @@ import "modules/font-awesome/css/font-awesome.min.css";
 import React from "react";
 import Todo from "../todo/todo";
 import About from "../about/about";
+import Menu from "../template/menu";
 
 // import { Container } from './styles';
 
 export default props => (
   <div className="container">
+    <Menu />
     <Todo />
     <About />
   </div>
