bb686cbcd7166abb2aa0edb45c399e526a07bc37
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2019-11-16 23:19:05 -0300

    axios delete

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -4,10 +4,9 @@ import axios from "axios";
 import PageHeader from "../template/pageHeader";
 import TodoForm from "../todo/todoForm";
 import TodoList from "../todo/todoList";
+import { pseudoRandomBytes } from "crypto";
 
-
-const URL = 'http://localhost:3003/api/todos'
-
+const URL = "http://localhost:3003/api/todos";
 
 export default class todo extends Component {
   constructor(props) {
@@ -18,24 +17,43 @@ export default class todo extends Component {
     };
     this.handleAdd = this.handleAdd.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.refresh();
   }
+
+  refresh() {
+    axios
+      .get(`${URL}?sort=-createdAt`)
+      .then(res =>
+        this.setState({ ...this.state, description: "", list: res.data })
+      );
+  }
+
   handleAdd() {
-    const description = this.state.description
-    axios.post(URL,{description}).then(res => console.log('Funcionou'))
+    const description = this.state.description;
+    axios.post(URL, { description }).then(res => this.refresh());
   }
+
   handleChange(e) {
     this.setState({ ...this.state, description: e.target.value });
   }
+
+  handleRemove(todo) {
+    axios
+      .delete(`${URL}/${todo._id}`)
+      .then(resp => this.refresh(this.state.description));
+  }
+
   render() {
     return (
       <div className="container-fluid">
         <PageHeader name="Tarefas" small="Cadastro" />
         <TodoForm
-          handleAdd={this.handleAdd}
           description={this.state.description}
+          handleAdd={this.handleAdd}
           handleChange={this.handleChange}
         />
-        <TodoList />
+        <TodoList list={this.state.list} handleRemove={this.handleRemove} />
       </div>
     );
   }
