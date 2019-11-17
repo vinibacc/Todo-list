5ffaf9ed66ee4d3adbd6bc514c9a53faf06369ca
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2019-11-16 21:50:19 -0300

    axios post

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,31 +1,36 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import PageHeader from "../template/pageHeader";
 import TodoForm from "../todo/todoForm";
 import TodoList from "../todo/todoList";
 
-// import { Container } from './styles';
+
+const URL = 'http://localhost:3003/api/todos'
+
 
 export default class todo extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      description:'',
-      list:[]
-    }
-    this.handleAdd = this.handleAdd.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+      description: "",
+      list: []
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   handleAdd() {
-    console.log(this.state.description)
+    const description = this.state.description
+    axios.post(URL,{description}).then(res => console.log('Funcionou'))
   }
   handleChange(e) {
-    this.setState({...this.state, description: e.target.value})
+    this.setState({ ...this.state, description: e.target.value });
   }
   render() {
     return (
       <div className="container-fluid">
         <PageHeader name="Tarefas" small="Cadastro" />
-        <TodoForm 
+        <TodoForm
           handleAdd={this.handleAdd}
           description={this.state.description}
           handleChange={this.handleChange}
